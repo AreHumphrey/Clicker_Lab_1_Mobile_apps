87df4ed530f6424dd574f14bd3f0ba1330dfae87
Dina <lutdina04@yandex.ru>
2023-11-10 12:01:39 +1000

    Смена строковых ресурсов

--- app/src/main/java/com/example/lab_11/LocaleHelper.java
+++ app/src/main/java/com/example/lab_11/LocaleHelper.java
@@ -5,13 +5,8 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
-import android.os.SharedMemory;
 import android.preference.PreferenceManager;
-import android.view.textclassifier.SelectionEvent;
-
 import java.util.Locale;
-import java.util.function.Predicate;
-import java.util.prefs.Preferences;
 
 public class LocaleHelper {
     private static final String SELECTED_LANGUAGE = "Locale.Helper.Selected.Language";
@@ -37,25 +32,11 @@ public class LocaleHelper {
         return context.createConfigurationContext(configuration);
     }
 
-    private static void persist(Context context, String language){
+    private static void persist(Context context, String language) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString(SELECTED_LANGUAGE, language);
         editor.apply();
 
-    }
-
-    private static Context updateResourcesLegacy(Context context, String language){
-        Locale locale = new Locale(language);
-        Locale.setDefault(locale);
-        Resources resources = context.getResources();
-
-        Configuration configuration = resources.getConfiguration();
-        configuration.locale = locale;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
-            configuration.setLayoutDirection(locale);
-        }
-        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
-        return context;
     }
 }
